e01e0e5317a3cf3b05045413b0f4cfa4dfb37bff
Eric Ulrich <eulrich@login.curnagl>
2021-10-12 16:25:55 +0200

    added scripts for illumina data structure

--- scripts/filesystem/copy_files.py
+++ scripts/filesystem/copy_files.py
@@ -2,6 +2,42 @@ import shutil
 import glob
 import pandas as pd
 import os
+from get_samples import Samples
+
+
+class Illumina():
+    def __init__(self):
+        self.target_dir = '/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data/'
+        self.src_dir = '/nas/FAC/FBM/DMF/smitri/evomicrocomm/D2c/00_ILLUMINA_DATA/02_TRIMMOMATIC/'
+        self.samples = Samples()
+
+    def create_dirs(self):
+        """Iterating over all Illumina samples and creating directories"""
+        for strain in self.samples.strains:
+            for sample in self.samples.strains[strain]:
+                if sample['platform'] == 'illumina':
+                    if not os.path.exists(sample['dir']):
+                        os.makedirs(sample['dir'])
+    
+    def copy_references(self):
+        """Copying references to every directory. Bit overkill but convenient."""
+        for strain in self.samples.strains:
+            for sample in self.samples.strains[strain]:
+                if sample['platform'] == 'illumina':
+                    if not os.path.exists(os.path.join(sample['dir'],'reference.fasta')):
+                        shutil.copyfile(self.samples.references[strain],os.path.join(sample['dir'],'reference.fasta'))
+
+    def copy_reads(self):
+        df = pd.read_csv('illumina_names.csv')
+        for fname,sample in zip(df['fname'],df['sample']):
+            reads = sorted(glob.glob(os.path.join(self.src_dir,fname+'*trimmed.fq.gz')))
+            names = ['read1.fq.gz','read2.fq.gz']
+            for read,name in zip(reads,names):
+                cmd = ['sbatch','copy_files.sh',read,\
+                    os.path.join(self.target_dir,sample,name)]
+                if not os.path.exists(os.path.join(self.target_dir,sample,name)):
+                    print(sample)
+                    shutil.copyfile(read,os.path.join(self.target_dir,sample,name))
 
 work = '/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data/illumina/'
 
