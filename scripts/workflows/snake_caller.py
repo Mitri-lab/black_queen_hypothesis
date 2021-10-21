ce26971bb19da03d7e1c066bf1a722b91c38ef99
nahanaoo <ericsven.u@gmail.com>
2021-10-21 14:36:09 +0200

    added single file caller

--- scripts/workflows/snake_caller.py
+++ scripts/workflows/snake_caller.py
@@ -12,6 +12,8 @@
 import subprocess
 from os.path import join
 from samples import Samples
+import glob
+import sys
 
 #Defining some globals
 work = '/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data/'
@@ -24,20 +26,26 @@ def submit(files):
     cmd = ['snakemake','--rerun-incomplete','-j','100',cluster_config,files]
     subprocess.call(' '.join(cmd),shell=True)
 
-def snake_test():
-    submit(join(work,'T22.4.5','oa','report.md'))
+def single_caller(path):
+    submit(path)
 
-def strain_caller(output_file):
+def strain_caller(strain,output_file):
     """Grabbing all the directories and formatting them as snakemake wildcards."""
     output = []
-    for sample in s.strains['Ochrobactrum anthropi']:
+    for sample in s.strains[strain]:
         if sample['platform'] == 'illumina':
             output.append(sample['name'])
     files = join(work,'{'+','.join(output)+'}',\
-        s.abbreviations['Ochrobactrum anthropi'],output_file)
+        s.abbreviations[strain],output_file)
     submit(files)
 
+def time_point_caller(output_file):
+    dirs = [d.split('/')[-1] for d in glob.glob(join(work,'T44.4.*'))]
+    submit(join(work,'{'+','.join(dirs)+'}','{at,ct,oa,ms}',output_file))
+
 if __name__ == '__main__':
-    #strain_caller(join('snippy','snps.tab'))
-    strain_caller('report.md')
+    #strain_caller(sys.argv[1],join('snippy','snps.tab'))
+    strain_caller(s.abbreviations[sys.argv[1]],'report.md')
     #snake_test()
+    #time_point_caller('report.md')
+    single_caller(sys.argv[1])
