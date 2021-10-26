dcd3325d435ca1fad07fa2d85350b560831775da
nahanaoo <ericsven.u@gmail.com>
2021-10-26 10:00:51 +0200

    switched to random line choice

--- scripts/analyze_snps.py
+++ scripts/analyze_snps.py
@@ -5,6 +5,7 @@ from mutations import Mutations
 import plotly.graph_objects as go
 import plotly.express as px
 from os.path import join
+import random
 
 e = Experiment()
 s = Samples()
@@ -13,7 +14,7 @@ m = Mutations()
 class Plotting():
     def plot_gene_series(self):
         m.get_gene_series()
-        styles = ['solid', 'dot', 'dash', 'longdash','solid','dashdot', 'longdashdot','solid']
+        styles = [ 'dot', 'dash', 'longdash','dashdot', 'longdashdot','solid']
         for (strain,treatment),df in m.gene_series.items():
             fig = go.Figure()
             for i,row in df.iterrows():
@@ -23,7 +24,7 @@ class Plotting():
                     name = i
                 ))
             for counter,trace in enumerate(fig.data):
-                trace.line.dash = styles[counter]
+                trace.line.dash = random.choice(styles)
             fig.update_layout(
                 title = ' '.join([strain,'in','treatment',str(treatment)]),
                 xaxis_title = 'timepoint',
