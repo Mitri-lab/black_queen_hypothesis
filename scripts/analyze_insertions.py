4994d5115b553c323fe45eea21f77adb29f57099
nahanoo <eric.ulrich@unil.ch>
2022-01-05 14:41:27 +0100

    renamed

--- scripts/analyze_insertions.py
+++ scripts/analyze_insertions.py
@@ -4,41 +4,22 @@ from glob import glob
 from os.path import join
 
 
-def parse_insertions(tsv):
-    return pd.read_csv(
-        tsv, sep="\t", usecols=["chromosome", "position", "length"]
-    ).drop_duplicates()
-
-
 def get_contigs(fasta):
     contigs = [contig for contig in SeqIO.parse(fasta, "fasta")]
-    c = {contig.id: len(contig) for contig in contigs}
+    c = {contig.id: contig.seq for contig in contigs}
     return c
 
 
-def create_df(fasta, insertions):
-    contigs = get_contigs(fasta)
-    insertions.insert(3, "contig_length", None)
-    for i, row in insertions.iterrows():
-        insertions.at[i, "contig_length"] = contigs[row.chromosome]
-    return insertions
-
-
-def all():
+def get_sequences():
     work = '/users/eulrich/work/genome_size/data'
-    tsvs = glob(join(work,'*','mutant_to_parent.noalignments.tsv'))
-    for tsv in tsvs:
-        fasta = tsv.replace('mutant_to_parent.noalignments.tsv','assembly.fasta')
-        insertions = parse_insertions(tsv)
-        df = create_df(fasta, insertions)
-        print(tsv.split('/')[-2],'##########################')
-        print(df)
-
-def single():
-    work = '/users/eulrich/work/genome_size/data'
-    sample = 'Ct43.1'
-    tsv = join(work,sample,'mutant_to_parent.noalignments.tsv')
-    fasta = join(work,sample,'assembly.fasta')
-    insertions = parse_insertions(tsv)
-    df = create_df(fasta, insertions)
-    print(df)
+    df = pd.read_csv("sequences_of_interest.csv")
+    df.insert(5,'sequence',None)
+    for i, row in df.iterrows():
+        c = row.chromosome
+        p = row.position
+        l = row.length
+        s = row.sample_name
+        fasta = join(work,s,'assembly.fasta')
+        contigs = get_contigs(fasta)
+        df.at[i,'sequence'] = str(contigs[c][p:p+l])
+    return df
