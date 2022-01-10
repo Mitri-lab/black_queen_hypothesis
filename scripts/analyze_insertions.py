315f447044c6ebdc26a3164303967522dec46043
nahanaoo <ericsven.u@gmail.com>
2022-01-10 13:14:48 +0100

    added plotting of filtered insertions

--- scripts/analyze_insertions.py
+++ scripts/analyze_insertions.py
@@ -3,12 +3,14 @@ import pandas as pd
 from glob import glob
 from os.path import join
 from samples import Samples
+from plotting import Plotting
 
+p = Plotting()
 s = Samples()
 
 
 def get_contigs(fasta):
-    """Parses fastas and returns dictionary with contig name as 
+    """Parses fastas and returns dictionary with contig name as
     key and sequence as value."""
     contigs = [contig for contig in SeqIO.parse(fasta, "fasta")]
     c = {contig.id: contig.seq for contig in contigs}
@@ -27,9 +29,11 @@ def parse_insertions():
                     sep="\t",
                     usecols=["chromosome", "position", "length"],
                 ).drop_duplicates()
-                df.insert(0, "sample_name", sample["name"])
-                df.insert(4, "sequence", None)
-                df.insert(5,'fasta',None)
+                df.insert(0,'strain',sample['strain'])
+                df.insert(1, "sample_name", sample["name"])
+                df.insert(2,'treatment',sample['treatment'])
+                df.insert(6, "sequence", None)
+                df.insert(7, "fasta", None)
                 fasta = join(sample["dir_name"], "assembly.fasta")
                 contigs = get_contigs(fasta)
                 for i, row in df.iterrows():
@@ -38,36 +42,48 @@ def parse_insertions():
                             row.position - 1 : row.position - 1 + row.length
                         ]
                     )
-                    df.at[i,'fasta'] = contigs
+                    df.at[i, "fasta"] = contigs
                 dfs.append(df)
     out = pd.concat(dfs)
     out.index = range(len(out))
     return out
 
 
-def filter_insertions(insertions,min_distance):
+def filter_insertions(insertions, min_distance):
     """To rule out assembly bias we look at insertion sequence
     which are not located at the end or the start of the assembly."""
     to_pop = []
     for i, row in insertions.iterrows():
-        contig_length = len(row['fasta'][row['chromosome']])
-        if row['length'] == contig_length:
+        contig_length = len(row["fasta"][row["chromosome"]])
+        if row["length"] == contig_length:
             pass
-        elif row['position'] < min_distance:
+        elif row["position"] < min_distance:
             to_pop.append(i)
-        elif contig_length - row['position'] < min_distance:
+        elif contig_length - row["position"] < min_distance:
             to_pop.append(i)
         else:
             pass
-    print('To drop')
     for element in to_pop:
-        row = insertions.loc[element]
-        print(row['sample_name'],row['chromosome'],row['position'],row['length'],len(row['fasta'][row['chromosome']]))
         insertions = insertions.drop(element)
-    print('To keep')
-    for i,row in insertions.iterrows():
-        print(row['sample_name'],row['chromosome'],row['position'],row['length'],len(row['fasta'][row['chromosome']]))
-    #return insertions
+    return insertions
+
+def plot_insertions(insertions):
+    for strain in s.strains:
+        df = insertions[insertions['strain'] == strain]
+        samples = set(df['sample_name'])
+        out = pd.DataFrame(0,columns=s.treatments[strain],index=samples)
+        for sample,treatment,length in zip(df['sample_name'],df['treatment'],df['length']):
+            out.at[sample,treatment] += length
+        fig = p.subplot_treatments(strain,out)
+        title = 'Filtered inserted bases in '+strain
+        fig.update_layout(
+            xaxis_title='samples',
+            yaxis_title='inserted bp',
+            title=title)
+        fig.update_traces(showlegend=False)
+        fig.write_image(join('..','plots','corrected_inserted_bases',title.replace(' ','_')+'.png'))
+
+
 
 def get_sequences():
     work = "/users/eulrich/work/genome_size/data"
