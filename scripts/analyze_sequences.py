816eba7471cdab171454c9f111a83432c899facc
nahanaoo <ericsven.u@gmail.com>
2022-01-11 16:57:23 +0100

    moved to object orientated code with Hgt class

--- scripts/analyze_sequences.py
+++ scripts/analyze_sequences.py
@@ -7,71 +7,55 @@ import pysam
 
 s = Samples()
 
+class Hgt():
+    def __init__(self,sample):
+        self.sample = sample
 
-def create_dirs():
-    for ancestral, samples in s.strains.items():
-        for sample in samples:
-            if sample["platform"] == "pacbio":
-                p = join(sample["dir_name"], "hgt")
-                if not exists(p):
-                    mkdir(p)
-                for strain in s.strains_per_treatment[sample["treatment"]]:
-                    reference = join(
-                        sample["dir_name"], "hgt", s.abbreviations[strain] + ".fasta"
-                    )
-                    if not exists(reference):
-                        symlink(
-                            s.references[strain],
-                            reference,
-                        )
+    def chunker(self,seq, window_size, step):
+        seqs = []
+        seqlen = len(seq)
+        for counter, i in enumerate(range(0, seqlen, step)):
+            j = seqlen if i + window_size > seqlen else i + window_size
+            chunk = seq[i:j]
+            chunk.id = chunk.id + "." + str(counter)
+            seqs.append(chunk)
+            if j == seqlen:
+                break
+        return seqs
+
+    def mapper(self):
+        for strain in s.strains_per_treatment[self.sample["treatment"]]:
+            reads = join(self.sample["dir_name"], "hgt", "chunked_sequences.fasta")
+            sam = join(self.sample["dir_name"], "hgt", s.abbreviations[strain] + ".sam")
+            cmd = [
+                "minimap2",
+                "-ax",
+                "asm5",
+                s.references[strain],
+                reads,
+                ">",
+                sam,
+            ]
+            call(" ".join(cmd), shell=True)
 
 
-def chunker(seq, window_size, step):
-    seqs = []
-    seqlen = len(seq)
-    for counter, i in enumerate(range(0, seqlen, step)):
-        j = seqlen if i + window_size > seqlen else i + window_size
-        chunk = seq[i:j]
-        chunk.id = chunk.id + "." + str(counter)
-        seqs.append(chunk)
-        if j == seqlen:
-            break
-    return seqs
-
-
-def mapper(sample):
-    for strain in s.strains_per_treatment[sample["treatment"]]:
-        reads = join(sample["dir_name"], "hgt", "chunked_reads.fasta")
-        sam = join(sample["dir_name"], "hgt", s.abbreviations[strain] + ".sam")
-        cmd = [
-            "minimap2",
-            "-ax",
-            "asm5",
-            s.references[strain],
-            reads,
-            ">",
-            sam,
+    def get_cross_mapping(self):
+        cross_strains = [
+            strain for strain in s.strains_per_treatment[self.sample['treatment']] if strain != self.sample["strain"]
         ]
-        call(" ".join(cmd), shell=True)
+        for cross_strain in cross_strains:
+            sam = join(self.sample["dir_name"], "hgt", s.abbreviations[cross_strain] + ".sam")
+            mapped_reads = []
+            a = pysam.AlignmentFile(sam, "rb")
+            for read in a:
+                if (not read.is_unmapped) & (not read.is_secondary):
+                    mapped_reads.append(read)
+                    print(read.seq)
+                    return True
+                    
 
 
-def get_cross_mapping(sample):
-    cross_strains = [
-        strain for strain in s.strains_per_treatment[sample['treatment']] if strain != sample["strain"]
-    ]
-    for cross_strain in cross_strains:
-        sam = join(sample["dir_name"], "hgt", s.abbreviations[cross_strain] + ".sam")
-        mapped_reads = []
-        a = pysam.AlignmentFile(sam, "rb")
-        for read in a:
-            if (not read.is_unmapped) & (not read.is_secondary):
-                mapped_reads.append(read)
-                print(read.seq)
-
-
-# sample = s.strains["Agrobacterium tumefaciens"][3]
-
-
+# self.sample = s.strains["Agrobacterium tumefaciens"][3]
 def test():
     for strain, samples in s.strains.items():
         for sample in samples:
@@ -88,4 +72,3 @@ def test():
                 mapper(sample)
                 get_cross_mapping(sample)
             
-test()
