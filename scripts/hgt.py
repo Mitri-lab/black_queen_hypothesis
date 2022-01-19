cb849e5b6c9b8259aa9cb9572263f539fc768dc4
nahanaoo <ericsven.u@gmail.com>
2022-01-19 09:51:21 +0100

    added docstrings

--- scripts/hgt.py
+++ scripts/hgt.py
@@ -18,11 +18,23 @@ class Hgt:
     are returned."""
 
     def __init__(self, sample):
+        # Dictionary with sample info from Sample class
         self.sample = sample
+        # Fasta of sample assembly
         fasta = join(self.sample['dir_name'], 'assembly.fasta')
+        # Contigs in dict form
         self.contigs = self.get_contigs(fasta)
+        # Step size for sliding windows algorithm
+        self.step = None
+        # Dictionary storing origins of sequence in assembly
+        self.origins = {key: dict() for key in self.contigs.keys()}
+        # Filtered origins
+        self.filtered = {key: dict() for key in self.contigs.keys()}
 
     def get_contig_names(self):
+        """Returns dictionary. contig_names with strains as keys
+        and contig names as values. reference_names with contig
+        names as keys and strain as value."""
         contig_names = {strain: None for strain in s.strains}
         reference_names = dict()
         for strain, reference in s.references.items():
@@ -41,7 +53,7 @@ class Hgt:
 
     def chunker(self, seq, window_size, step):
         """Creates chunks of a sequence. window_size defines
-        chunk size and step the amount of basepair the windows
+        chunk size and step the amount of basepairs the window
         is moved forward."""
         # List which stores all chunks
         seqs = []
@@ -56,24 +68,28 @@ class Hgt:
             seqs.append(chunk)
             if j == seqlen:
                 break
-        # Writes chunked sequence to hgt directory
         return seqs
 
     def chunk_assembly(self):
+        """Chunks an assembly of multiple contigs into different 
+        chunks using a sliding window algorightm (see chunker function)."""
         assembly_chunks = []
         for name, contig in self.contigs.items():
             record = SeqRecord(contig, id=name)
+            # Creates chunks of every contig
             assembly_chunks += self.chunker(record, 500, 100)
         target = join(self.sample["dir_name"], "hgt",
                       "chunked_sequences.fasta")
+        # Dumps chunks to fasta
         with open(target, "w") as handle:
             SeqIO.write(assembly_chunks, handle, "fasta")
 
     def mapper(self):
         """Maps chunked sequence to all ancesteral genomes
-        present in experiment with minimap2.
+        experiment with minimap2.
         Minimap2 settings are set to accureate PacBio reads."""
         for strain in s.strains.keys():
+            # Query sequences created with chunk_assembly()
             reads = join(self.sample["dir_name"],
                          "hgt", "chunked_sequences.fasta")
             sam = join(self.sample["dir_name"], "hgt",
@@ -87,47 +103,59 @@ class Hgt:
                 ">",
                 sam,
             ]
+            # Calling minimap and surpressing stdout
             call(" ".join(cmd), shell=True, stdout=subprocess.DEVNULL,
                  stderr=subprocess.STDOUT)
 
     def get_mapping_stats(self):
         """Checks all mapped sequences and returns contig name
         of reference."""
-        mapped_sequences = {key: dict() for key in self.contigs.keys()}
         c_names, r_names = self.get_contig_names()
         for strain in s.strains.keys():
+            # Iterating over every sam file
             sam = join(
                 self.sample["dir_name"], "hgt", s.abbreviations[strain] + ".sam"
             )
             a = pysam.AlignmentFile(sam, "rb")
             reads = []
-            # Iterating over all mapped primary reads
+            # Iterating over all reads
+            # Read must be primary,mapped and have quality of 60
             for read in a:
                 if (not read.is_unmapped) & (not read.is_secondary) & (read.mapq == 60):
                     reads.append(read)
             # Appends contig name of reference
             for read in reads:
+                # Contig and position of query sequence form assembly are
+                # stored in contig name
+                # First part is contig name, second number is n step
+                # step size is known and position therefore as well
                 name = '.'.join(read.qname.split('.')[:-1])
                 pos = int(read.qname.split('.')[-1])*self.step
+                # Iteratign over aligned query sequence
                 for j in range(pos+read.query_alignment_start, pos+read.query_alignment_end):
-                    if j not in mapped_sequences[name].keys():
-                        mapped_sequences[name][j] = []
-                        mapped_sequences[name][j].append(
+                    # Checking if position already in dictionary
+                    if j not in self.origins[name].keys():
+                        self.origins[name][j] = []
+                        # Appending strain at given position
+                        self.origins[name][j].append(
                             r_names[read.reference_name])
 
                     else:
-                        mapped_sequences[name][j].append(
+                        # Appending strain at given position
+                        self.origins[name][j].append(
                             r_names[read.reference_name])
-        # Returns set of contig names of references
-        self.mapped_sequences = mapped_sequences
 
     def dump_origins(self):
-        self.filtered = {key: dict() for key in self.contigs.keys()}
-        for name, contig in self.mapped_sequences.items():
+        """Filters identified origins and dumps to json. Only positions
+        with either two different origins or an origin which is not anceteral
+        are of interest"""
+        for name, contig in self.origins.items():
             for pos, strains in contig.items():
+                # Applying filter
                 if (len(set(strains)) > 1) or (strains[0] != self.sample['strain']):
                     self.filtered[name][pos] = list(set(strains))
 
+        # Dumping to json
         j_f = json.dumps(self.filtered, indent=4)
         with open(join(self.sample['dir_name'], 'origins.json'), 'w') as handle:
             handle.write(j_f)
