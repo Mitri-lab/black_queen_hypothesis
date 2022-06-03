f9b99247a9f93cc83d2eb6b92c552059c5779a0d
nahanaoo <ericsven.u@gmail.com>
2022-06-03 16:11:21 +0200

    added freq plots

--- scripts/trajectories.py
+++ scripts/trajectories.py
@@ -41,13 +41,13 @@ class SNPs:
                             print(f)
                     sample['snps'] = snps
                     self.snps[strain].append(sample)
-    
+
     def join_genbank(self):
         genbanks = {}
         for strain in s.strains:
-            f = s.references[strain].replace('.fasta','_stripped.gbk')
+            f = s.references[strain].replace('.fasta', '_stripped.gbk')
             contigs = [contig for contig in SeqIO.parse(
-            f, 'genbank')]
+                f, 'genbank')]
             genbank = {contig.id: {} for contig in contigs}
             for contig in contigs:
                 for feature in contig.features:
@@ -62,54 +62,73 @@ class SNPs:
             genbanks[strain] = genbank
         for strain, samples in self.snps.items():
             for sample in samples:
-                pass
-        return genbanks
+                for snp in sample['snps']:
+                    snp['product'] = 'intergenic'
+                    for (start, end), product in genbanks[strain][snp['chrom']].items():
+                        if (snp['pos'] >= start) & (snp['pos'] <= end):
+                            snp['product'] = product
 
-
-    def plot_phred_score(self):
-        df = pd.DataFrame(list(), columns=['cosm', 'qual', ])
-        """for i in df.index:
-            df.at[i,'qual'] = []"""
+    def get_data_frame(self,strain,filter_genes=False):
+        self.df = pd.DataFrame(
+            columns=['micro_treat', 'timepoint', 'freq', 'qual', 'color', 'product'])
         i = 0
-        for sample in self.snps[s.abbreviations['at']]:
-            for snp in sample['snps']:
-                df.at[i, 'qual'] = snp['qual']
-                df.at[i, 'cosm'] = str(
-                    sample['treatment']) + '.' + str(sample['cosm'])
-                i += 1
-        fig = px.histogram(df, x='qual', facet_col='cosm', log_y=True,
-                           facet_col_wrap=4, title='SNP phred scores')
-        fig.write_image('hist_20.png', scale=2)
-
-    def plot_trajectories(self):
-        df = pd.DataFrame(columns=['micro_treat', 'timepoint', 'freq','qual','color'])
-        i = 0
-        for sample in self.snps[s.abbreviations['at']]:
+        for sample in self.snps[strain]:
             for snp in sample['snps']:
                 key = '.'.join([str(item) for item in [snp['chrom'],
-                            snp['pos'], sample['treatment'], sample['cosm']]])
-                df.at[i, 'freq'] = snp['freq_sum']
-                df.at[i, 'micro_treat'] = str(
+                                                       snp['pos'], sample['treatment'], sample['cosm']]])
+                self.df.at[i, 'freq'] = snp['freq_sum']
+                self.df.at[i, 'micro_treat'] = str(
                     sample['treatment']) + '.' + str(sample['cosm'])
-                df.at[i, 'timepoint'] = sample['timepoint']
-                df.at[i,'qual'] = snp['qual']
-                df.at[i,'color'] = key
+                self.df.at[i, 'timepoint'] = sample['timepoint']
+                self.df.at[i, 'qual'] = snp['qual']
+                self.df.at[i, 'color'] = key
+                self.df.at[i, 'product'] = snp['product']
                 i += 1
+
+        if filter_genes:
+            self.df = self.df.drop_duplicates(['product','timepoint','micro_treat'])
+
+    def plot_phred_score(self,out):
+        fig = px.histogram(self.df, x='qual', facet_col='micro_treat', log_y=True,
+                           facet_col_wrap=3, title='SNP phred scores',
+                           category_orders={'micro_treat': list(sorted(self.df['micro_treat']))})
+        fig.write_html(out + '_phred_score.html')
+
+    def plot_trajectories(self,out):
         mask = []
-        for i in df['color']:
+        for i in self.df['color']:
             if 'AGTU001.0001.c01.2351' in i:
                 mask.append(True)
             else:
                 mask.append(False)
         #df = df[mask]
-        fig = px.line(df, x='timepoint', y='freq',color='color', facet_col='micro_treat',facet_col_wrap=3)
-        fig.update_xaxes(categoryorder='array', categoryarray= ['T11','T22','T33','T44'])
-        return df
+        fig = px.line(self.df, x='timepoint', y='freq', color='color',
+                      facet_col='micro_treat', facet_col_wrap=3,
+                      hover_data=['product'],
+                      category_orders={'micro_treat': list(sorted(self.df['micro_treat']))})
+        fig.update_xaxes(categoryorder='array', categoryarray=[
+                         'T11', 'T22', 'T33', 'T44'])
+        fig.update_traces(showlegend=False)
+        fig.write_html(out + '_trajectories.html')
 
-    
+    def plot_frequencies(self,out):
+        fig = px.histogram(self.df, x='freq', facet_col='micro_treat', log_y=True,
+                           facet_col_wrap=3, title='SNP frequencies',
+                           category_orders={'micro_treat': list(sorted(self.df['micro_treat']))})
+        fig.write_html(out + '_frequencies.html')
 
-snps = SNPs(filter=None)
-# snps.plot_phred_score()
+snps = SNPs(filter=20)
+snps.join_genbank()
+for strain in s.strains:
+    out = s.abbreviations[strain]
+    snps.get_data_frame(strain)
+    snps.plot_phred_score(out)
+    snps.plot_frequencies(out)
+    snps.plot_trajectories(out)
+    snps.get_data_frame(strain,filter_genes=True)
+    out = 'unique_genes_' + out
+    snps.plot_frequencies(out)
+    snps.plot_trajectories(out)
 """freqs = snps.plot_trajectories()
 cosms = set(freqs['micro_treat'])
 for cosm in cosms:
