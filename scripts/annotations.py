8bc6a8cdeba5f299e947e937aa987e277d723d1d
nahanaoo <ericsven.u@gmail.com>
2023-08-07 11:08:10 +0200

    added sequences to annotation

--- scripts/annotations.py
+++ scripts/annotations.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from os.path import join
 from samples import Samples
-
+from Bio import SeqIO
 s = Samples()
+
 
 def annotate_clusters(abb):
     """Prints annotations for identified SNPs."""
     def annotate_pos(gbk, c, p):
         for (start, end), (gene, product) in gbk[c].items():
             if p in range(start, end):
-                return [gene, product]
+                return [(start, end), gene, product]
         return False
 
     f = join('..', 'annotations', abb + '_renamed.tsv')
@@ -32,26 +33,53 @@ def annotate_clusters(abb):
             product = row['Product']
         gbk[row['Sequence Id']][(row['Start'], row['Stop'])] = (gene, product)
 
-    out = pd.DataFrame(columns=['chrom', 'pos', 'gene', 'product','treatment','timepoint','cosm'])
-    cluster = pd.read_csv(join('..', 'variants', 'snps_freebayes_comp_mapping.csv'))
+    out = pd.DataFrame(columns=['chrom', 'start', 'end',
+                       'gene', 'product', 'treatment', 'timepoint', 'cosm'])
+    cluster = pd.read_csv(
+        join('..', 'variants', 'snps_freebayes_comp_mapping.csv'))
     cluster = cluster[cluster['strain'] == s.abbreviations[abb]]
-    for i,row in cluster.iterrows():
-        a = annotate_pos(gbk, row['chrom'],row['pos'])
+    for i, row in cluster.iterrows():
+        a = annotate_pos(gbk, row['chrom'], row['pos'])
         if a:
             pass
         else:
-            a = ['Not annotated', 'Not annotated']
-        out.loc[i] = [row['chrom'],row['pos'],a[0],a[1],row['treatment'],row['timepoint'],row['cosm']]
-    out.to_csv(join('..','annotations',abb+'_snps_annotations.csv'),index=False)
+            a = [('Not annotated', 'Not annotated'),
+                 'Not annotated', 'Not annotated']
+        out.loc[i] = [row['chrom'], a[0][0], a[0][1], a[1], a[2],
+                      row['treatment'], row['timepoint'], row['cosm']]
+    out.insert(len(out.columns), 'sequence', None)
+    contigs = {contig.name: str(contig.seq) for contig in SeqIO.parse(
+        join('..', 'annotations', abb+'.fasta'), 'fasta')}
+    for i, row in out.iterrows():
+        if row['gene'] != 'Not annotated':
+            seq = contigs[row['chrom']][int(row['start']):int(row['end'])]
+        else:
+            seq = 'Not annotated'
+        out.at[i, 'sequence'] = seq
+    out.to_csv(join('..', 'annotations', abb +
+               '_snps_annotations.csv'), index=False)
+    return out
 
-f = join('..','annotations','at_snps_annotations.csv')
-df = pd.read_csv(f)
-df = df[df['timepoint'] == 'T44']
-products = set(df['product'])
-out = pd.DataFrame(columns=['Gene','Product','Treatments'])
-for p in products:
-    tmp = df[df['product'] == p]
-    if len(tmp) > 1:
-        tmp.index = range(len(tmp))
-        ts = ', '.join([str(e) for e in (list(set(tmp['treatment'])))])
-        out.loc[len(out)] = [tmp.loc[0]['gene'],p,ts]
+def selection():
+    fs = [join('..', 'annotations', 'at_snps_annotations.csv'),
+        join('..', 'annotations', 'ct_snps_annotations.csv')]
+    outs = []
+    for f in fs:
+        print(f)
+        df = pd.read_csv(f)
+        df = df[df['timepoint'] == 'T44']
+        products = set(df['product'])
+        out = pd.DataFrame(
+            columns=['Gene', 'Start', 'End','Count', 'Product', 'Treatments', 'Sequence'])
+        for p in products:
+            tmp = df[df['product'] == p]
+            if len(tmp) > 1:
+                tmp.index = range(len(tmp))
+                ts = ', '.join([str(e) for e in (list(set(tmp['treatment'])))])
+                counts = len(tmp)
+                out.loc[len(out)] = [tmp.loc[0]['gene'], tmp.loc[0]['start'],
+                                    tmp.loc[0]['end'],counts, p, ts, tmp.loc[0]['sequence']]
+        outs.append(out)
+    return outs
+
+
