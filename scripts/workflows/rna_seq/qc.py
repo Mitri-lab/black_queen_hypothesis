fe7b297435037b1e0fb752f0578431ac9bac012b
nahanaoo <ericsven.u@gmail.com>
2023-08-25 13:26:51 +0200

    changed plot sizes

--- scripts/workflows/rna_seq/qc.py
+++ scripts/workflows/rna_seq/qc.py
@@ -4,6 +4,7 @@ from samples import Samples
 import pandas as pd
 from os.path import split, join
 from Bio import SeqIO
+import math
 s = Samples()
 
 
@@ -42,10 +43,9 @@ ct2_ext = ['03685','20235','06455','11590','24860','02180','25795','04460','0655
 ct2 = ['02180','20240','06555','20235','18275','06560','04460','10260']
 
 
-
-def annnotate(genes):
+def annotate(names):
     products = []
-    genes = ['NHFMJM_' + g for g in genes]
+    genes = ['NHFMJM_' + g for g in names]
     f = 'ct.assembly.contigs.polypolish.transcript.gtf'
     columns = ['contig', 'source', 'type', 'start',
             'stop', 'a', 'strand', 'b', 'product']
@@ -57,18 +57,33 @@ def annnotate(genes):
     gbk = [contig for contig in SeqIO.parse(f, 'genbank')]
     for contig in gbk:
         for feature in contig.features:
+            entry = [None,None]
             try:
-                features[feature.qualifiers['locus_tag'][0]
-                        ] = feature.qualifiers['product']
+                entry[0] = feature.qualifiers['product'][0]
+            except KeyError:
+                entry[0] = 'Unknown'
+            try:
+                entry[1] = feature.qualifiers['gene'][0]
+            except KeyError:
+                entry[1] = 'Unknown'
+            try:
+                features[feature.qualifiers['locus_tag'][0]] = entry
             except KeyError:
                 pass
-
+    annot = pd.read_csv('deg_CtA_Ct2.tsv', sep='\t')
+    annot.index = annot['genes']
     for p in df['product']:
         for g in genes:
             if g in p:
-                print(g,features[g][0])
-                products.append(features[g][0])
-    return products
+                fc = annot.loc[g]['logFC']
+                pv = -math.log10(annot.loc[g]['FDR'])
+                entry = (g,features[g][1],features[g][0],fc,pv)
+                products.append(entry)
+    out = pd.DataFrame(columns=['Gene ID','Gene','Product','Log2 fold change','-Log10P'])
+    for i in products:
+        out.loc[len(out)] = list(i)
+    return out
 
-Ct4 = annnotate(gs_ext)
-Ct2 = annnotate(ct2)
+df = annotate(ct2)
+caption = 'Differentially expressed genes in mono-evolved \textit{C. testosteroni}'
+print(df.to_latex(index=False,caption=caption))
